c8f0ef138ba541f88d88fcd323360e115960a9f9
JackeyLea <1768478912@qq.com>
2019-09-13 21:40:17 +0800

    调整文件位置

--- 4.复试/3.上机/C++程序设计-陈家骏/示例代码/4-1.cpp
+++
@@ -1,5 +0,0 @@
-int factorial(int n)
-{	int i,f=1;
-	for (i=2; i<=n; i++) f *= i;
-	return f;
-}
--- 4.复试/3.上机/C++程序设计-陈家骏/示例代码/4-7.cpp
+++
@@ -1,5 +0,0 @@
-unsigned int random()
-{	static unsigned int seed=1;
-	seed = (25173*seed+13849)%65536;
-	return seed;
-}
